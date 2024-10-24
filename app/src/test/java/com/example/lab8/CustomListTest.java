d23a14579f650627b0b0c325aaa7ab2e0c950857
Ekam Rajveer Singh Kumar <ekamrajv@ualberta.ca>
2024-10-24 17:46:28 -0600

    Added Add City test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -5,12 +5,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
+
+    public CustomList MockCityList(){
+        CustomList list = new CustomList(null, new ArrayList<>());
+        return list;
+    }
+
+    @Test
+    public void addCityTest(){
+        CustomList list = MockCityList();
+        int expectedCountBeforeAdd = 0;
+        assertEquals(expectedCountBeforeAdd, list.getCount(), "Count should be not zero.");
+        City cityToAdd = new City("Nanton", "AB");
+        list.add(cityToAdd);
+
+        int expectedCountAfterAdd = 1;
+        assertEquals(expectedCountAfterAdd, list.getCount(), "The city was not added.");
+    }
 
 
 }
